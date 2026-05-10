461fa895d0c37b76eb7d74c58d93d0bbe20a1847
thekingofspace <37231416+thekingofspace@users.noreply.github.com>
2026-05-09 22:13:57 -0700

    1.0.0 release

--- Ruzit/cli/src/update.rs
+++ Ruzit/cli/src/update.rs
@@ -25,7 +25,7 @@ fn steam_lib_name() -> &'static str {
 fn agent() -> Agent {
     ureq::AgentBuilder::new()
         .timeout(std::time::Duration::from_secs(60))
-        .user_agent("ruzit/4.6")
+        .user_agent("ruzit/1.0.0")
         .build()
 }
 
